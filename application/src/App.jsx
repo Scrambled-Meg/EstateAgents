278ac6c47102f1f14fe1d4002bf8823ba28460e6
Meg-Scott-Digital <megan.scott2@engineering.digital.dwp.gov.uk>
2025-04-23 13:57:03 +0100

    Menu setup as per Shafeeq demo

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -1,13 +1,19 @@
-
+import Menu from './components/Menu'
 
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
      <h1> Team A's Estate Agents </h1>
-        
+     
+
+      <Menu/>
+
+     
+      
     </>
   )
 }
---
+++ application/src/components/Menu.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Link, Router} from "react-router-dom"
+
+function Menu(){
+    
+    return(
+    <>
+    <BrowserRouter>
+
+        <Link> Link 1 </Link>
+        <Link> Link 2 </Link>
+        <Link> Link 3 </Link>
+
+    </BrowserRouter>
+    
+
+    </>
+    )
+}
+
+export default Menu
